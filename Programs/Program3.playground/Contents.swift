d6e7ef25ea65876390cf26dbb93ca7e10ef3cb7d
Lokesh Vyas <lokesh.vyas@gonuclei.com>
2022-02-09 16:20:16 +0530

    jj

---
+++ Programs/Program3.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+//struct Toppr {
+//    fileprivate(set) var employee = "Rohan"
+//}
+//
+//var toppr = Toppr()
+//toppr.employee = "Rohit"
+//
+//print(toppr.employee)
+
+// Output: Rohit
+
+
+//-------------------------------------------------------------------------------
+//enum FootballTeams: CaseIterable {
+//    case Barcelona
+//    case Manchester(type: Int)
+//    case Liverpool
+//      case Chelsea
+//}
+//
+//print(FootballTeams.allCases.count)
+
+// Output: won't exexute as Manchester(type: Int) are not supported by CaseIterable
+
+//-------------------------------------------------------------------------------
+//struct ManchesterUnited {
+//    static var manager = "Ralf Rangnick"
+//    var playerName: String
+//    var playerAge: Int
+//}
+//
+//let utd = ManchesterUnited(playerName: "Ronaldo", playerAge: 36)
+//print(utd.manager)
+
+// OutPut : ManchesterUnited.manager
+
+//-------------------------------------------------------------------------------
+
+//let numbers = [1, 3, 5 ]
+//let result = numbers.reduce(3, +)
+//print(result)
+
+// Output : 12
+
+//-------------------------------------------------------------------------------
+//func topprDivider<T:BinaryInteger>(_ value: T ) -> T {
+//    return value / value
+//}
+//
+//print(topprDivider(5))
+
+// Output: won't run as operator '/' cannot be applied to two 'T' operands, for this topprDivider<T> should be topprDivider<T:BinaryInteger>(_ value: T )
--- iOS/MyDispatchQueue.playground/Contents.swift
+++ iOS/MyDispatchQueue.playground/Contents.swift
@@ -1,22 +1,55 @@
 import UIKit
 
 class MyDispatchGroup {
-
+    
     private var groupCounter = 0
     private var completionBlock: (() -> ())?
-
+    
     func enter() {
         groupCounter += 1
     }
-
+    
     func leave() {
         groupCounter -= 1
         if groupCounter == 0 {
             completionBlock?()
         }
     }
-
+    
     func notify(completion: @escaping () -> ()) {
         completionBlock = completion
     }
 }
+
+
+let queue = DispatchQueue.global()
+let concuQ = DispatchQueue(label: "com.ss.sssaw", attributes: .concurrent)
+let serialQ = DispatchQueue(label: "com.ss.ssss")
+
+func print1() {
+    //    print("1")
+    //
+    //    queue.async {
+    //        print("2")
+    //        queue.sync {
+    //            print("3")
+    //        }
+    //        print("4")
+    //    }
+    //    print("5")
+    
+    queue.async {
+        concuQ.async {
+            for i in 1...5 {
+                print(i)
+            }
+        }
+        serialQ.async {
+            for i in 6...10 {
+                print(i)
+            }
+        }
+    }
+}
+
+print1()
